5700b416c62c5055747e4562e6e28c3a01f7e411
valushagrinchik <valushagrinchik@gmail.com>
2026-05-26 15:08:08 +0300

    return back web service

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await NestFactory.createApplicationContext(AppModule);
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
